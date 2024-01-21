dbae5d9f2d7016f482db5b1f901d320a978d85fd
oleg.m <olegmarko7@gmail.com>
2024-01-21 01:27:48 +0100

    [~] mailer service

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
@@ -14,7 +17,9 @@ const webPort = "80"
 func main() {
 	log.Println("Starting mail service on port: ", webPort)
 
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -25,4 +30,21 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+	}
+
+	return m
 }
